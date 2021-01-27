d3213ffc8469d3ffd5ae4dd4d4c14b83c4c72a3e
MichaelSpielmann <77295035+MichaelSpielmann@users.noreply.github.com>
2021-01-27 18:17:11 +0100

    fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
-
-const regexPatterns = {
-  password: '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}'
-};
+import { FormBuilder, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,38 +8,30 @@ const regexPatterns = {
 })
 export class LoginComponent implements OnInit {
 
-  loginFormGroup = new FormGroup ({
-    email: new FormControl(),
-    password: new FormControl(),
+  loginFormGroup = this.formBuilder.group({
+    email: [null,
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+      ]
+    ],
+    password: [
+      null,
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}')
+      ],
+    ],
   });
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.createForm();
   }
 
   submit(): void {}
 
-  createForm(): void {
-
-    this.loginFormGroup = this.formBuilder.group({
-      email: [null,
-        [
-          Validators.required,
-          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
-        ]
-      ],
-      password: [
-        null,
-        [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,}')
-        ],
-      ],
-    });
-  }
 
 }
 
