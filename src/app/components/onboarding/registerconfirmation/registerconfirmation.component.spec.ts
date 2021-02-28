a04caad48662e18523ac8bf6120e0b479a961b13
MichaelSpielmann <77295035+MichaelSpielmann@users.noreply.github.com>
2021-02-28 15:18:23 +0100

    fix merge errors

--- src/app/components/onboarding/registerconfirmation/registerconfirmation.component.spec.ts
+++ src/app/components/onboarding/registerconfirmation/registerconfirmation.component.spec.ts
@@ -1,16 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { RegisterConfirmationComponent } from './registerconfirmation.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpLoaderFactory } from '../../../app.module';
+import { HttpClient} from '@angular/common/http';
 
 describe('RegisterConfirmationComponent', () => {
   let component: RegisterConfirmationComponent;
   let fixture: ComponentFixture<RegisterConfirmationComponent>;
 
+  let translate: TranslateService;
+  let http: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterConfirmationComponent ]
+      declarations: [ RegisterConfirmationComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ]
     })
     .compileComponents();
+    translate = TestBed.inject(TranslateService);
+    http = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
--- src/app/components/onboarding/registerconfirmation/registerconfirmation.component.ts
+++ src/app/components/onboarding/registerconfirmation/registerconfirmation.component.ts
@@ -1,15 +1,21 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-registerconfirmation',
+  selector: 'arpa-registerconfirmation',
   templateUrl: './registerconfirmation.component.html',
   styleUrls: ['./registerconfirmation.component.css']
 })
 export class RegisterConfirmationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/onboarding/login']);
   }
 
 }
