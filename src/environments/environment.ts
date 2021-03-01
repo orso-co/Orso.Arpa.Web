2289a19e7edac27d994af028d172994b260776c2
Sascha Seewald <sascha.seewald@me.com>
2021-03-01 18:59:11 +0100

    ARPA-155 Use local backend by default (#37)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   api: {
-    protocol: 'https',
-    baseUrl: 'orso-arpa.azurewebsites.net'
+    protocol: 'http',
+    baseUrl: 'localhost:5000'
   },
   web:
   {
