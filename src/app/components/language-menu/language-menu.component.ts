324140c1c3a241cfdb644d18d69a47a9db9c02e9
kypu <kylie.pusch@gmail.com>
2021-03-04 16:12:33 +0100

    language switch

--- src/app/components/language-menu/language-menu.component.ts
+++ src/app/components/language-menu/language-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {SelectItem} from 'primeng/api';
 
 @Component({
   selector: 'arpa-language-menu',
@@ -8,19 +9,22 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class LanguageMenuComponent implements OnInit {
 
-  languages: Array<string>;
+  languages: Array<SelectItem<string>> = [];
   currentLanguage: string;
 
   constructor(private translate: TranslateService) {
-    this.languages = translate.getLangs();
+    translate.getLangs().forEach(language => {
+      console.log(language);
+      this.languages.push({ value: language });
+    });
     this.currentLanguage = translate.currentLang;
   }
 
   ngOnInit(): void {
   }
 
-  updateLanguage(langChoice: any): void {
-    this.translate.use(langChoice.option);
+  updateLanguage(): void {
+    this.translate.use(this.currentLanguage);
   }
 
 
