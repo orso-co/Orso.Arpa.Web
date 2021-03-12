715ce103b79d4a5fe89e26f198adf628c5670bc6
Wolfgang Roese <mail@wolfgangroese.de>
2021-03-12 22:10:28 +0100

    modifiey spec-files for testing

--- src/app/components/forbidden/forbidden.component.spec.ts
+++ src/app/components/forbidden/forbidden.component.spec.ts
@@ -3,6 +3,11 @@ import { waitForAsync } from '@angular/core/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
 
 import { ForbiddenComponent } from './forbidden.component';
 
@@ -12,7 +17,18 @@ describe('ForbiddenComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ForbiddenComponent ]
+      declarations: [ ForbiddenComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader:{
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
     })
     .compileComponents();
   }));
