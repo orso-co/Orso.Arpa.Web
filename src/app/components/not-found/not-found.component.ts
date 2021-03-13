9e54105b63ab994bc7dfaf4fbe7ffae4bc0f897a
Wolfgang Roese <mail@wolfgangroese.de>
2021-03-13 17:24:26 +0100

    minor fix viewport height & gitignore /out

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -5,13 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit  {
-
-  constructor() { }
-
-ngOnInit(): void {
-}
-
+export class NotFoundComponent  {
 
 
 }
