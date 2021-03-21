d9a188dcdb365dae7565be55ab00ed58a2cbaa42
iskaleade <google@integritysquared.de>
2021-03-21 13:05:45 +0100

    added some edit functionality to profile page

--- src/app/components/onboarding/profile/profile.component.ts
+++ src/app/components/onboarding/profile/profile.component.ts
@@ -1,20 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MeService } from 'src/app/services/me.service';
+import { IUserProfileDto } from 'src/app/models/IUserProfileDto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SubSink } from 'subsink';
+import { ToastService } from 'src/app/services/toast.service';
+
 
 @Component({
   selector: 'arpa-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor(
-    private router: Router,
-  ) { }
+  private subs = new SubSink();
+  userProfile: IUserProfileDto;
+  profileFormGroup: FormGroup;
+
+  constructor(private meService: MeService,
+              private router: Router,
+              private fb: FormBuilder,
+              private toastService: ToastService
+  ) {
+    this.profileFormGroup = this.fb.group({
+      userName : [{value: '', disabled: true}],
+      givenName: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.pattern('[- +()0-9]+')]
+    });
+   }
 
   ngOnInit(): void {
+    this.subs.add(this.meService.getMyProfile().subscribe(data => {
+      this.userProfile = data;
+      this.profileFormGroup.setValue(this.userProfile);
+    }));
+
+    this.subs.add(this.profileFormGroup.valueChanges.subscribe(data => {
+      this.userProfile = data;
+    }));
   }
+
+  // workaround to display formGroup fields in original order
+  returnZero(): number {
+    return 0;
+  }
+
   goToDashboard(): void {
     this.router.navigate(['/pages/dashboard']);
-    }
   }
+
+  onSubmit(): void {
+    this.subs.add(this.meService.putProfile(this.userProfile).subscribe(data => {
+      if (data == null ) {
+        this.toastService.info('profile.UPDATE');
+      }
+      else {
+        this.toastService.error('profile.ISSUE');
+      }
+    }
+    ));
+  }
+
+  ngOnDestroy(): void{
+    this.subs.unsubscribe();
+  }
+}
