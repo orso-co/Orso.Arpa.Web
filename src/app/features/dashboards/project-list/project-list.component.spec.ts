99cd102e6d9b1a858f054ee3af8eb179e9f5516b
sSeewald <sascha.seewald@me.com>
2021-04-01 08:06:02 +0200

    ARPA-250: refactoring project structure

--- src/app/features/dashboards/project-list/project-list.component.spec.ts
+++ src/app/features/dashboards/project-list/project-list.component.spec.ts
@@ -1,11 +1,6 @@
 import { waitForAsync } from '@angular/core/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectListComponent } from './project-list.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateMockModule } from '../../../../testing/translate.mock.module';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { DummyComponent } from '../../../shared/dummy/dummy.component';
 
 describe('ProjectListComponent', () => {
   let component: ProjectListComponent;
@@ -14,26 +9,15 @@ describe('ProjectListComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule,
-          TranslateMockModule,
-        ],
-        declarations: [ProjectListComponent, DummyComponent],
-        providers: [{
-          provide: ActivatedRoute,
-          useValue: {
-            data: of({ projects: [] }),
-          },
-        }],
+        declarations: [ProjectListComponent],
+        providers: [],
       }).compileComponents();
-    }),
+    })
   );
-
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectListComponent);
     component = fixture.componentInstance;
-    component.projects = [];
     fixture.detectChanges();
   });
 
--- src/app/main/layout/page-header/page-header.component.spec.ts
+++ src/app/main/layout/page-header/page-header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<< HEAD:src/app/main/layout/page-header/page-header.component.spec.ts
 import { PageHeaderComponent } from './page-header.component';
 
 describe('PageHeaderComponent', () => {
@@ -9,12 +10,27 @@ describe('PageHeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PageHeaderComponent],
+=======
+import { PrivacyComponent } from './privacy.component';
+
+describe('PrivacyComponent', () => {
+  let component: PrivacyComponent;
+  let fixture: ComponentFixture<PrivacyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrivacyComponent ]
+>>>>>>> ARPA-250: refactoring project structure:src/app/main/privacy/privacy.component.spec.ts
     })
       .compileComponents();
   });
 
   beforeEach(() => {
+<<<<<<< HEAD:src/app/main/layout/page-header/page-header.component.spec.ts
     fixture = TestBed.createComponent(PageHeaderComponent);
+=======
+    fixture = TestBed.createComponent(PrivacyComponent);
+>>>>>>> ARPA-250: refactoring project structure:src/app/main/privacy/privacy.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
