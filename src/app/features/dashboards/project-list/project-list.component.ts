a3592df7faa556bef71a694cb00a16aca734d238
sSeewald <sascha.seewald@me.com>
2021-04-02 22:39:33 +0200

    ARPA-250: move loader responsibility to application level

--- src/app/features/dashboards/project-list/project-list.component.ts
+++ src/app/features/dashboards/project-list/project-list.component.ts
@@ -1,4 +1,3 @@
-import { SubSink } from 'subsink';
 import { Component, Input, OnDestroy } from '@angular/core';
 import { IProjectDto } from '../../../models/appointment';
 import { ActivatedRoute } from '@angular/router';
@@ -9,23 +8,16 @@ import { map } from 'rxjs/operators';
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
 })
-export class ProjectListComponent implements OnDestroy {
+export class ProjectListComponent {
   @Input() projects: IProjectDto[] | null = [];
-  private subs = new SubSink();
 
   constructor(
     route: ActivatedRoute,
   ) {
     if (route) {
-      this.subs.add(
-        route.data.pipe(map((routeData) => routeData.projects))
-          .subscribe((project) => (this.projects = this.filterActiveProjects(project))),
-      );
+      route.data.pipe(map((routeData) => routeData.projects))
+        .subscribe((project) => (this.projects = this.filterActiveProjects(project)));
     }
-  }
-
-  ngOnDestroy(): void {
-    this.subs.unsubscribe();
   }
 
   filterActiveProjects(projects: IProjectDto[]): IProjectDto[] {
