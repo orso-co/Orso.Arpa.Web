29638f47e85f9de41e9dd0d907aad1ac68774e34
kypu <kylie.pusch@gmail.com>
2021-04-25 16:19:02 +0200

    fixed tests

--- src/app/features/projects/edit-project/edit-project.component.spec.ts
+++ src/app/features/projects/edit-project/edit-project.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditProjectComponent } from './edit-project.component';
+import {DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {PrimeNgModule} from '../../../shared/prime-ng/prime-ng.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {httpLoaderFactory} from '../../../core/core.module';
+import {HttpClient} from '@angular/common/http';
 
 describe('EditProjectComponent', () => {
   let component: EditProjectComponent;
@@ -8,7 +16,25 @@ describe('EditProjectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditProjectComponent ]
+      declarations: [ EditProjectComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        PrimeNgModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient],
+          },
+        }),
+      ],
+      providers: [
+        { provide: DynamicDialogRef, useValue: {} },
+        { provide: DynamicDialogConfig, useValue: { data: { projects: [], venues: [] } } },
+      ],
     })
     .compileComponents();
   });
