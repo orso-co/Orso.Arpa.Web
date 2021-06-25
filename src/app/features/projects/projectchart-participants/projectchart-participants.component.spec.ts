a0625fe02980b47d4a7d36ad6d3ee2841ab87037
Wolfgang Roese <mail@wolfgangroese.de>
2021-06-25 17:42:20 +0200

    arpa-407_dummychart_project-participation

---
+++ src/app/features/projects/projectchart-participants/projectchart-participants.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectchartParticipantsComponent } from './projectchart-participants.component';
+
+describe('ProjectchartParticipantsComponent', () => {
+  let component: ProjectchartParticipantsComponent;
+  let fixture: ComponentFixture<ProjectchartParticipantsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectchartParticipantsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectchartParticipantsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
