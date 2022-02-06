a5217f9bc785344701f71893048af23e97615ac0
Wolfgang Roese <mail@wolfgangroese.de>
2022-02-06 14:21:00 +0100

    website-keys updated (#77)

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   captcha: {
-    key: '6Lf-AkIdAAAAAEIr-TKvo3KayGf4qTqwmsOuxMLH',
+    key: '6Le-g3QaAAAAAJmIN-s37441yuZPBp1M7nqUwtED',
   },
   api: {
     protocol: 'https',
