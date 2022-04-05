63caae9f4cabc5a9d9a3a6c89f9e2d58581dc7a9
Sascha Seewald <sascha.seewald@novael.de>
2022-04-05 22:31:13 +0200

    * encoding and formatting issue

--- src/app/features/person-dialog/person-contactdata/person-contactdata.component.ts
+++ src/app/features/person-dialog/person-contactdata/person-contactdata.component.ts
@@ -1,16 +1,14 @@
 import { SelectValueService } from './../../../shared/services/select-value.service';
 import { ContactDetailDto } from './../../../../@arpa/models/contactDetailDto';
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NotificationsService } from 'src/@arpa/services/notifications.service';
 import { ColumnDefinition } from 'src/@arpa/components/table/table.component';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { cloneDeep } from 'lodash-es';
 import { first, map } from 'rxjs/operators';
 import { SelectItem } from 'primeng/api';
 import { PersonDto } from 'src/@arpa/models/personDto';
 import { ContactService } from '../services/contact.service';
-import { resolve } from '@apidevtools/json-schema-ref-parser';
 
 @Component({
   selector: 'arpa-person-contactdata',
@@ -29,7 +27,13 @@ export class PersonContactdataComponent implements OnInit {
     { label: '#', property: 'key', type: 'template', template: 'keyTemplate' },
     { label: 'persons.contact.VALUE', property: 'value', type: 'text' },
     { label: 'persons.contact.PREFERENCE', property: 'preference', type: 'rating', show: true },
-    { label: 'persons.contact.TYPE', property: 'typeId', type: 'state', stateTable: 'ContactDetail', stateProperty: 'Type' },
+    {
+      label: 'persons.contact.TYPE',
+      property: 'typeId',
+      type: 'state',
+      stateTable: 'ContactDetail',
+      stateProperty: 'Type',
+    },
     { label: 'persons.contact.COMMENT_INNER', property: 'commentInner', type: 'text', show: false },
     { label: 'persons.contact.COMMENT_TEAM', property: 'commentTeam', type: 'text', show: false },
   ];
@@ -39,11 +43,12 @@ export class PersonContactdataComponent implements OnInit {
     { icon: 'pi pi-phone', value: 2 },
     { icon: 'pi pi-link', value: 3 },
   ];
+
   constructor(
     private formBuilder: FormBuilder,
     private contactService: ContactService,
     private notificationsService: NotificationsService,
-    private selectValueService: SelectValueService
+    private selectValueService: SelectValueService,
   ) {
     this.form = this.formBuilder.group({
       key: [null, [Validators.required]],
@@ -57,7 +62,7 @@ export class PersonContactdataComponent implements OnInit {
   }
 
   ngOnInit() {
-    if(this.person ) {
+    if (this.person) {
       this.tableData.next(this.person.contactDetails);
     }
     this.typeOptions$ = this.selectValueService
@@ -66,38 +71,38 @@ export class PersonContactdataComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.person){
-    const { id, key, value, typeId, commentInner, preference } = this.form.getRawValue();
-    if (id) {
-      this.contactService
-        .updateContactDetail(id, { key, value, typeId, commentInner, preference: preference || 0 })
-        .pipe(first())
-        .subscribe((_) => {
-          const index = this._tableData.findIndex((el) => el.id === id);
-          this._tableData[index] = { ...this._tableData[index], key, value, typeId, commentInner, preference };
-          this.tableData.next(this._tableData);
-          this.notificationsService.success('CONTACT_DETAIL_MODIFIED', 'contact');
-          this.form.reset({});
-        });
-    } else {
-      this.contactService
-        .addContactDetail(this.person.id, { id, key, value, typeId, commentInner, preference: preference || 0 })
-        .pipe(first())
-        .subscribe((result) => {
-          // this.tableData.next(this.person?.contactDetails?.push(result));
-          this.notificationsService.success('CONTACT_DETAIL_ADDED', 'contact');
-          this.form.reset({});
-        });
+    if (this.person) {
+      const { id, key, value, typeId, commentInner, preference } = this.form.getRawValue();
+      if (id) {
+        this.contactService
+          .updateContactDetail(id, { key, value, typeId, commentInner, preference: preference || 0 })
+          .pipe(first())
+          .subscribe((_) => {
+            const index = this._tableData.findIndex((el) => el.id === id);
+            this._tableData[index] = { ...this._tableData[index], key, value, typeId, commentInner, preference };
+            this.tableData.next(this._tableData);
+            this.notificationsService.success('CONTACT_DETAIL_MODIFIED', 'contact');
+            this.form.reset({});
+          });
+      } else {
+        this.contactService
+          .addContactDetail(this.person.id, { id, key, value, typeId, commentInner, preference: preference || 0 })
+          .pipe(first())
+          .subscribe((result) => {
+            this.tableData.next(this.person?.contactDetails?.push(result));
+            this.notificationsService.success('CONTACT_DETAIL_ADDED', 'contact');
+            this.form.reset({});
+          });
+      }
     }
   }
-}
 
   remove(contactDetail: ContactDetailDto): void {
     this.contactService
       .deleteContactDetail(contactDetail.id)
       .pipe(first())
       .subscribe(() => {
-        // this.tableData.next(this.person?.contactDetails?.filter((e) => e.id != contactDetail.id));
+        this.tableData.next(this.person?.contactDetails?.filter((e) => e.id != contactDetail.id));
         this.notificationsService.success('CONTACT_DETAIL_REMOVED', 'contact');
       });
   }
