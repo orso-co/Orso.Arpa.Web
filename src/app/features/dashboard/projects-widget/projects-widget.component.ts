bc4f3b356fbd91377962deecf7f7ea406e8135a6
Wolfgang Roese <mail@wolfgangroese.de>
2022-12-20 22:01:48 +0100

    added roleName check to show staffroles other projectwidget

--- src/app/features/dashboard/projects-widget/projects-widget.component.ts
+++ src/app/features/dashboard/projects-widget/projects-widget.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectDto } from '@arpa/models';
+import { ProjectDto, UserDto } from '@arpa/models';
 import { shareReplay } from 'rxjs/operators';
-import { ApiService, AuthService } from '@arpa/services';
+import { ApiService } from '@arpa/services';
 
 @Component({
   selector: 'arpa-projects-widget',
@@ -10,16 +10,21 @@ import { ApiService, AuthService } from '@arpa/services';
 })
 export class ProjectsWidgetComponent implements OnInit {
   allProjects: ProjectDto[] = [];
+  user: UserDto;
   private baseUrl = '/api';
+
+  public isStaff(): boolean {
+    return this.user.roleNames!.includes('staff');
+  }
 
   constructor(
     private apiService: ApiService,
-    private AuthService: AuthService
   ) {}
 
   ngOnInit(): void {
     this.apiService.get<ProjectDto[]>(`${this.baseUrl}/projects`).pipe(shareReplay());
   }
+
 
   getProjectNames(projects: ProjectDto[]): string {
     return projects.map((p) => p.title).join(', ');
