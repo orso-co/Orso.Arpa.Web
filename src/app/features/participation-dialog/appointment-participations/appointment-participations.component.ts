bcd78554bcfcd05aa45e2541e2c48ed55d483adb
Wolfgang Roese <mail@wolfgangroese.de>
2023-02-23 19:02:55 -0300

    added table with appointments-participation-status in project-participation-dialog of a given mupro

--- src/app/features/participation-dialog/appointment-participations/appointment-participations.component.ts
+++ src/app/features/participation-dialog/appointment-participations/appointment-participations.component.ts
@@ -1,23 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppointmentParticipationsService } from './appointmentparticipations.service';
-import { ProjectParticipationDto } from '@arpa/models';
+import { AppointmentDto, ProjectParticipationDto } from '@arpa/models';
+import { ColumnDefinition } from '../../../../@arpa/components/table/table.component';
 
 @Component({
   selector: 'arpa-appointment-participations',
   templateUrl: './appointment-participations.component.html',
   styleUrls: ['./appointment-participations.component.scss']
 })
-export class AppointmentParticipationsComponent {
+export class AppointmentParticipationsComponent implements OnChanges {
   @Input() participation: ProjectParticipationDto;
   @Input() projectId: string;
   @Input() musicianId: string;
   appointmentParticipations$: Observable<any>;
+  columns: ColumnDefinition<AppointmentDto>[] = [
+    { label: 'APPOINTMENT', property: 'appointment.name', type: 'text' },
+    { label: 'BEGIN', property: 'appointment.startTime', type: 'date' },
+    { label: 'PREDICTION', property: 'appointmentParticipation.prediction', type: 'text' },
+    { label: 'RESULT', property: 'appointmentParticipation.result', type: 'text' },
 
+
+  ];
   constructor(
     private appointmentParticipationsService: AppointmentParticipationsService
-  ) {
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.projectId || changes.musicianId) {
       this.appointmentParticipations$ = this.appointmentParticipationsService.getByProjectId(this.projectId, this.musicianId);
     }
+  }
+
 
 }
--- src/app/features/participation-dialog/participation-dialog.module.ts
+++ src/app/features/participation-dialog/participation-dialog.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ParticipationDialogComponent } from './components/participation-dialog/participation-dialog.component';
-import { ParticipationEditComponent } from './components/participation-edit/participation-edit.component';
+import { ParticipationDialogComponent } from './participation-dialog.component';
+import { ParticipationEditComponent } from './participation-edit/participation-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { LayoutModule } from '../../layout/layout.module';
+import { LayoutModule } from '../../../@arpa/layout/layout.module';
 import { ListboxModule } from 'primeng/listbox';
 import { DropdownModule } from 'primeng/dropdown';
 import { RatingModule } from 'primeng/rating';
@@ -14,9 +14,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { BadgeModule } from 'primeng/badge';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { StepsModule } from 'primeng/steps';
-import { FormFieldModule } from '../form-field/form-field.module';
+import { FormFieldModule } from '../../../@arpa/components/form-field/form-field.module';
 import { TabViewModule } from 'primeng/tabview';
-import { AppointmentEditComponent } from './components/appointment-edit/appointment-edit.component';
+import { AppointmentParticipationsComponent } from './appointment-participations/appointment-participations.component';
+import { TableModule } from '../../../@arpa/components/table/table.module';
 
 @NgModule({
   imports: [
@@ -36,11 +37,12 @@ import { AppointmentEditComponent } from './components/appointment-edit/appointm
     DropdownModule,
     FormFieldModule,
     TabViewModule,
+    TableModule,
   ],
   declarations: [
     ParticipationDialogComponent,
     ParticipationEditComponent,
-    AppointmentEditComponent
+    AppointmentParticipationsComponent
   ],
   exports: [ParticipationDialogComponent],
 })
