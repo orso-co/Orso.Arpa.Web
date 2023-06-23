fcdb8466638a00c8946a5b2536c0b709c16e8a93
Wolfgang Roese <wolfgang.roese@orso.co>
2023-06-23 17:23:21 +0200

    user menue adjustments (#245)

--- src/@arpa/components/theme-switcher/theme-switcher.module.ts
+++ src/@arpa/components/theme-switcher/theme-switcher.module.ts
@@ -5,17 +5,12 @@ import { ThemeSwitcherComponent } from './theme-switcher.component';
 import { ThemeSwitcherService } from './theme-switcher.service';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { FormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    SelectButtonModule,
-    InputSwitchModule,
-  ],
+  imports: [CommonModule, FormsModule, SelectButtonModule, InputSwitchModule, ToggleButtonModule],
   declarations: [ThemeSwitcherComponent],
   providers: [ThemeSwitcherService],
   exports: [ThemeSwitcherComponent],
 })
-export class ThemeSwitcherModule {
-}
+export class ThemeSwitcherModule {}
