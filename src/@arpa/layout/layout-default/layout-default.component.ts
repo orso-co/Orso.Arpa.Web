69b4cf4bdeb88da5bf4cd3f5071baf5f6a1534b4
wolfgangroese <wolfgang.roese@orso.co>
2023-06-23 14:34:52 +0200

    user menue adjustments

--- src/@arpa/layout/layout-default/layout-default.component.ts
+++ src/@arpa/layout/layout-default/layout-default.component.ts
@@ -1,4 +1,4 @@
-import { MenuService } from './../../components/menu/menu.service';
+import { MenuService } from '../../components/menu/menu.service';
 import { Component, ElementRef, OnDestroy, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
@@ -115,11 +115,11 @@ export class LayoutDefaultComponent implements OnDestroy {
           translationToken: 'PERSONAL',
           children: [
             {
-              label: 'Meine Termine',
-              icon: 'pi pi-calendar',
-              roles: ['performer', 'staff'],
-              routerLink: '/arpa/profile/appointments',
-              translationToken: 'MY_APPOINTMENTS',
+              label: 'Meine Daten',
+              icon: 'pi pi-user-edit',
+              roles: ['performer', 'staff', 'admin'],
+              routerLink: '/arpa/profile/user',
+              translationToken: 'MY_DATA',
             },
             {
               label: 'Meine Projekte',
@@ -128,27 +128,19 @@ export class LayoutDefaultComponent implements OnDestroy {
               routerLink: '/arpa/profile/my-projects',
               translationToken: 'MY_PROJECTS',
             },
-
             {
-              label: 'Mein Musikprofil',
+              label: 'Meine Termine',
+              icon: 'pi pi-calendar',
+              roles: ['performer', 'staff'],
+              routerLink: '/arpa/profile/appointments',
+              translationToken: 'MY_APPOINTMENTS',
+            },
+            {
+              label: 'Meine Profile',
               icon: 'pi pi-user-edit',
               roles: ['performer'],
               routerLink: '/arpa/profile/musician',
               translationToken: 'MY_PROFILES',
-            },
-            {
-              label: 'Meine Daten',
-              icon: 'pi pi-user',
-              roles: ['performer', 'staff', 'admin'],
-              routerLink: '/arpa/profile/user',
-              translationToken: 'MY_DATA',
-            },
-            {
-              label: 'Mein QR-Code',
-              icon: 'pi pi-sign-in',
-              roles: ['performer'],
-              routerLink: '/arpa/profile/qrcode',
-              translationToken: 'MY_QRCODE',
             },
           ],
         },
