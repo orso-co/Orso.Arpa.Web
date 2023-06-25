822da3190ff1d354ac51409d09743497890559dc
wolfgangroese <wolfgang.roese@orso.co>
2023-06-25 23:47:13 +0200

    styling and cleanup

--- src/app/features/dashboard/news-widget/news-widget.component.ts
+++ src/app/features/dashboard/news-widget/news-widget.component.ts
@@ -1,8 +1,7 @@
-import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { NewsService } from '../../../../@arpa/services/news.service';
 import { Observable, of } from 'rxjs';
 import { NewsDto } from '@arpa/models';
-import { map } from 'rxjs/operators';
 import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
@@ -12,14 +11,12 @@ import { LazyLoadEvent } from 'primeng/api';
 })
 export class NewsWidgetComponent {
   news$: Observable<NewsDto[]> = of([]);
-  totalRecordsCount$: Observable<number> = of(0);
   pageSize = 4;
   selectedNewsItem: NewsDto | undefined;
 
   constructor(private newsService: NewsService) {}
 
   loadData(event: LazyLoadEvent): void {
-    console.log('load data', event);
     this.news$ = this.newsService.getAllNews(this.pageSize, 0);
   }
 
