0191ff7d3ad120277668472a1b43a343ab9869ed
Maximilian Jakob Maag <derjambus@animexx.de>
2024-02-26 16:56:42 +0100

    Testing e2e contact information

--- cypress/e2e/user journeies/user_personal_info.cy.ts
+++ cypress/e2e/user journeies/user_personal_info.cy.ts
@@ -24,9 +24,54 @@ describe('User can manage his contact info', () => {
         cy.get('#p-tabpanel-3-label').click()
 
         // Assert
-
+        cy.get('.hasHover > :nth-child(2)').contains('max.mustermann@gmail.com')
+        cy.get('.hasHover > :nth-child(4)').contains('business')
+        cy.get('.hasHover > :nth-child(5)').contains('Testcomment written by cypress, only usefully for a test with cypress.')
     })
 
-    //TODO
-    // change personal data, delete personal data
+    it('Change contact information', () => {
+        // Arrange
+        cy.clearAllCookies()
+        cy.login('admin', 'Pa$$w0rd')
+        cy.get(':nth-child(5) > .p-ripple > .p-menuitem-icon').click()
+        cy.get('#p-tabpanel-3-label').click()
+        cy.get('[aria-labelledby="pi pi-envelope"]').click()
+        cy.get('.p-my-5 > .p-grid > :nth-child(2) > .form-field-appearance-outline > .p-field > .form-field-wrapper > .form-field-flex > .form-field-infix > .p-element > .p-dropdown > .p-dropdown-label')
+        .click()
+        cy.get('[icon="pi pi-pencil"]').click()
+        cy.get('#value').clear()
+        cy.get('#value').type('max.mustermann@outlook.de')
+        cy.get(':nth-child(5) > .form-field-appearance-outline > .p-field > .form-field-wrapper > .form-field-flex > .form-field-infix > #commentInner')
+        .clear()
+        cy.get(':nth-child(5) > .form-field-appearance-outline > .p-field > .form-field-wrapper > .form-field-flex > .form-field-infix > #commentInner')
+        .type('Changed testcomment written by cypress, only usefully for a test with cypress.')
+
+        // Act
+        cy.get('arpa-user-contact-data > .p-my-5 > .p-formgroup-inline > .p-text-left > .p-element')
+        .click()
+        cy.logout()
+
+        // Assert
+        cy.login('admin', 'Pa$$w0rd')
+        cy.get(':nth-child(5) > .p-ripple > .p-menuitem-icon').click()
+        cy.get('#p-tabpanel-3-label').click()
+        cy.get('[aria-labelledby="pi pi-envelope"]').click()
+        cy.get('.p-my-5 > .p-grid > :nth-child(2) > .form-field-appearance-outline > .p-field > .form-field-wrapper > .form-field-flex > .form-field-infix > .p-element > .p-dropdown > .p-dropdown-label')
+        .click()
+        cy.get('.hasHover > :nth-child(2)').contains('max.mustermann@outlook.de')
+        cy.get('.hasHover > :nth-child(5)')
+        .contains('Changed testcomment written by cypress, only usefully for a test with cypress.')
+    })
+
+    it('Delete contact information', () => {
+       cy.clearAllCookies()
+        cy.login('admin', 'Pa$$w0rd')
+        cy.get(':nth-child(5) > .p-ripple > .p-menuitem-icon').click()
+        cy.get('#p-tabpanel-3-label').click()
+        cy.get('[aria-labelledby="pi pi-envelope"]').click()
+        cy.get('.p-my-5 > .p-grid > :nth-child(2) > .form-field-appearance-outline > .p-field > .form-field-wrapper > .form-field-flex > .form-field-infix > .p-element > .p-dropdown > .p-dropdown-label')
+        .click()
+        cy.get('.end > [icon="pi pi-trash"] > .p-button-icon').click()
+        cy.logout()
+    })
 })
