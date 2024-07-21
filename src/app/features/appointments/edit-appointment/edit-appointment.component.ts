715bfaf51c21b7a8dea137b5909c07b8d8e4656a
wolfgangroese <wolfgang.roese@orso.co>
2024-07-21 16:59:40 +0200

    changed from stepper to tabview with all appointment-details in one view. adjusted saving-behavior onSubmit. fixed  column issue in participant list.

--- src/app/features/appointments/edit-appointment/edit-appointment.component.ts
+++ src/app/features/appointments/edit-appointment/edit-appointment.component.ts
@@ -13,7 +13,7 @@ import {
 } from '@arpa/models';
 import { TranslateService } from '@ngx-translate/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ConfirmationService, MenuItem, SelectItem } from 'primeng/api';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { sortBy, uniq } from 'lodash-es';
@@ -58,6 +58,7 @@ class ParticipationTableItem {
   selector: 'arpa-edit-appointment',
   templateUrl: './edit-appointment.component.html',
   styleUrls: ['./edit-appointment.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EditAppointmentComponent implements OnInit {
   @ViewChild('participationTable') table: Table;
@@ -124,9 +125,8 @@ export class EditAppointmentComponent implements OnInit {
       { field: 'prediction', header: this.translate.instant('appointments.PREDICTION'), width: '20%' },
       { field: 'result', header: this.translate.instant('appointments.RESULTS'), width: '20%' },
       { field: 'sections', header: this.translate.instant('appointments.SECTIONS'), width: '20%' },
-      // { field: 'qualification', header: this.translate.instant('appointments.QUALIFICATION'), width: '20%' },
+      { field: 'qualification', header: this.translate.instant('appointments.QUALIFICATION'), width: '20%' },
     ];
-    this.createStepperMenu();
   }
 
   private loadData() {
@@ -213,11 +213,6 @@ export class EditAppointmentComponent implements OnInit {
       this.venueOptions = this.venues?.map((v) => this.mapVenueToSelectItem(v));
       this.setRooms(this.appointment.venueId);
       this.ready = true;
-
-      // Check if appointment.participations is loaded
-      console.log('Appointment Participations:', this.appointment.participations);
-
-      // Calculate totalParticipationCount
       this.calculateTotalParticipationCount();
     });
   }
@@ -226,35 +221,37 @@ export class EditAppointmentComponent implements OnInit {
     this.totalParticipationCount = this.appointment.participations?.length || 0;
   }
 
-  onSubmit(continueToNextStep: boolean): void {
+  onSubmit(): void {
     if (this.formGroup.invalid || this.formGroup.pristine) {
+      this.notificationsService.info('No changes to save');
       return;
     }
     if (this.isNew) {
-      this.createAppointment({ ...this.appointment, ...this.formGroup.value }, continueToNextStep);
+      this.createAppointment({ ...this.appointment, ...this.formGroup.value });
     } else {
-      this.updateAppointment(
-        {
-          id: this.appointment.id,
-          categoryId: this.appointment.categoryId,
-          startTime: this.appointment.startTime,
-          endTime: this.appointment.endTime,
-          name: this.appointment.name,
-          publicDetails: this.appointment.publicDetails,
-          internalDetails: this.appointment.internalDetails,
-          status: this.appointment.status,
-          salaryId: this.appointment.salaryId,
-          salaryPatternId: this.appointment.salaryPatternId,
-          expecationId: this.appointment.expectationId,
-          ...this.formGroup.value,
-        },
-        continueToNextStep
-      );
+      this.updateAppointment({
+        id: this.appointment.id,
+        categoryId: this.appointment.categoryId,
+        startTime: this.appointment.startTime,
+        endTime: this.appointment.endTime,
+        name: this.appointment.name,
+        publicDetails: this.appointment.publicDetails,
+        internalDetails: this.appointment.internalDetails,
+        status: this.appointment.status,
+        salaryId: this.appointment.salaryId,
+        salaryPatternId: this.appointment.salaryPatternId,
+        expecationId: this.appointment.expectationId,
+        ...this.formGroup.value,
+      });
     }
   }
 
-  onActiveIndexChange(event: number) {
-    if (event === 2 && !this.areParticipationsAlreadyLoaded) {
+  cancelAndClose(): void {
+    this.ref.close();
+  }
+
+  onTabChange(event: { index: number }) {
+    if (event.index === 1 && !this.areParticipationsAlreadyLoaded) {
       this.ready = false;
       this.appointmentService.getById(this.appointment.id, true).subscribe(
         (appointment) => {
@@ -283,10 +280,7 @@ export class EditAppointmentComponent implements OnInit {
 
             this.mapParticipations();
             this.ready = true;
-
             this.calculateTotalParticipationCount();
-            // Check if appointment.participations is loaded
-            console.log('Appointment Participations:', this.appointment.participations);
           }
         },
         () => (this.ready = true)
@@ -298,32 +292,27 @@ export class EditAppointmentComponent implements OnInit {
     return { label: `${venue?.address?.city} ${venue?.address?.urbanDistrict} | ${venue?.name}`, value: venue?.id };
   }
 
-  updateAppointment(appointment: AppointmentModifyBodyDto, continueToNextStep: boolean): void {
+  updateAppointment(appointment: AppointmentModifyBodyDto): void {
     this.appointmentService
       .update(appointment)
       .pipe(first())
       .subscribe(() => {
         this.notificationsService.success('appointments.APPOINTMENT_UPDATED');
-        if (continueToNextStep) {
-          this.appointment = { ...this.appointment, ...appointment };
-          this.fillForm();
-          this.activeIndex = 1;
-        } else {
-          this.ref.close(appointment);
-        }
+        this.appointment = { ...this.appointment, ...appointment };
+        this.fillForm();
+        this.activeIndex = 0;
       });
   }
 
-  createAppointment(appointment: AppointmentCreateDto, continueToNextStep: boolean): void {
+  createAppointment(appointment: AppointmentCreateDto): void {
     this.appointmentService
       .create(appointment)
       .pipe(first())
       .subscribe((result) => {
         this.notificationsService.success('appointments.APPOINTMENT_CREATED');
-        if (continueToNextStep) {
+        if (onsubmit) {
           this.appointment = result;
           this.fillForm();
-          this.createStepperMenu();
           this.activeIndex = 1;
         } else {
           this.ref.close(result);
@@ -538,6 +527,10 @@ export class EditAppointmentComponent implements OnInit {
       salaryId: [null],
       salaryPatternId: [null],
       expectationId: [null],
+      venueId: [null],
+      roomId: [null],
+      projectIds: [null],
+      sectionIds: [null],
     });
   }
 
@@ -569,39 +562,11 @@ export class EditAppointmentComponent implements OnInit {
     });
   }
 
-  private createStepperMenu(): void {
-    this.items = [
-      {
-        label: this.translate.instant('appointments.BASICDATA'),
-        command: () => {
-          this.activeIndex = 0;
-        },
-      },
-      {
-        label: this.translate.instant('appointments.ADDITIONALDATA'),
-        disabled: this.isNew,
-        command: () => {
-          this.activeIndex = 1;
-        },
-      },
-      {
-        label: this.translate.instant('appointments.RESULTS'),
-        disabled: this.isNew,
-        command: () => {
-          this.activeIndex = 2;
-        },
-      },
-    ];
-  }
-
   private deleteAppointment(): void {
     this.appointmentService.delete(this.appointment.id).subscribe(() => {
       this.notificationsService.success('appointments.APPOINTMENT_DELETED');
       this.ref.close(this.appointment.id);
     });
-  }
-  exportCSV() {
-    this.table.exportCSV();
   }
 
   public onSendNotification(event: Event) {
@@ -645,7 +610,10 @@ export class EditAppointmentComponent implements OnInit {
   getSendNotificationLabel(): string {
     const sendNotificationLabel = this.translate.instant('appointments.SEND_NOTIFICATION');
     const recipientsLabel = this.translate.instant('RECEPIENTS');
-
-    return `${sendNotificationLabel} (${this.totalParticipationCount} ${recipientsLabel})`;
+    if (this.totalParticipationCount === 0) {
+      return `${sendNotificationLabel}`;
+    } else {
+      return `${sendNotificationLabel} (${this.totalParticipationCount} ${recipientsLabel})`;
+    }
   }
 }
